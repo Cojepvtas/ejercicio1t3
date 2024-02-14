1696deb458e6736b3d0de562835385bdeb50ea29
itzaFc <itzaFc@DESKTOP-G0ERNLI>
2024-02-14 10:34:16 -0600

    Agregar archivos de proyecto.

---
+++ ejercicio1t3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio1t3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+
+            Console.WriteLine("Ingrese un número entero:");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            if (numero % 2 == 0)
+            {
+                Console.WriteLine("El número ingresado es PAR.");
+            }
+            else
+            {
+                Console.WriteLine("El número ingresado es IMPAR.");
+            }
+
+            Console.WriteLine("Precione x para salir.");
+            Console.ReadKey();
+
+        }
+    }
+}
